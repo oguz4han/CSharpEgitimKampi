59180c0df491fa3277cf4e3075fc832c87c5d298
Oğuzhan ÇAKMAK <83668054+oguz4han@users.noreply.github.com>
2024-09-30 13:01:49 +0300

    Add project files.

---
+++ 01_MainSubjects/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_MainSubjects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region YazdırmaKomutlari
+
+            Console.WriteLine("11");
+
+            #endregion
+
+            int musteriNumarasi = 152;
+
+        }
+
+
+
+    }
+}
