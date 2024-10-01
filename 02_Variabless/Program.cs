97c70d787124c496022cd97f6fb52202cc0d4642
Oğuzhan ÇAKMAK <83668054+oguz4han@users.noreply.github.com>
2024-10-01 20:59:11 +0300

    Ders2 bitti

---
+++ 02_Variabless/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Variabless
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region MyRegion
+            double sayi = 45.8;
+            Console.WriteLine(sayi);
+
+            #endregion
+
+
+
+
+            int elmaFiyat = 15;
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Elmanın fiyati : "+elmaFiyat+" ₺");
+
+
+            #region Char Değişkenler
+
+
+
+            #endregion
+
+            #region Klavyeden Veri Girişler
+
+            Console.Write("Adınızı giriniz :");
+            string ad;
+            ad = Console.ReadLine();
+
+            Console.WriteLine("Merhaba "+ad+" Hoşgeldiniz...");
+
+            #endregion
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
